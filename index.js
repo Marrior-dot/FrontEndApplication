eee6789c14249b2a40a95a49457dc6d4337ad18e
Marrior <mrlimaroberto@gmail.com>
2024-06-22 19:35:08 -0300

    tentando gerar coisas dnâmicas

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+var path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'/src/views/'))
+
+app.get('/', (req, res) => {
+    let quantity = 0
+    res.render('index', {quantity:quantity})
+})
+
+app.listen(8000, (req,res) => {
+    console.log("Aplicação sendo executado na porta 8000")
+})
